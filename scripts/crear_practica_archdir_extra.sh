55c0b4c8f383ab7264ad6a2c442c7197d9f81587
Manuel Arroyo Salcedo <manuel.arroyo.profe@gmail.com>
2026-04-06 10:46:40 +0200

    Script creación de estructura para practica extra de comandos sobre archivos y directorios

---
+++ scripts/crear_practica_archdir_extra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p archivos_extra/documentos
+mkdir -p archivos_extra/copias
+mkdir -p archivos_extra/temporal
+mkdir -p archivos_extra/documentos/teoria/linux
+mkdir -p archivos_extra/documentos/teoria/windows
+
+touch archivos_extra/documentos/tema1.txt
+touch archivos_extra/documentos/tema2.txt
+touch archivos_extra/documentos/notas.txt
+touch archivos_extra/temporal/borrador.txt
+
+echo "Práctica de archivos y directorios creada."
