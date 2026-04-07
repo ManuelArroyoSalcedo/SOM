97bca371bba24d7bfae9288265df880e9171955d
Manuel Arroyo Salcedo <manuel.arroyo.profe@gmail.com>
2026-04-07 10:40:07 +0200

    Script crear_practica_empaquetado_extra.sh

---
+++ scripts/crear_practica_empaquetado_extra.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p practica_compresion/documentos/cartas
+mkdir -p practica_compresion/documentos/informes
+mkdir -p practica_compresion/imagenes
+mkdir -p practica_compresion/copias
+touch practica_compresion/documentos/cartas/carta1.txt
+touch practica_compresion/documentos/cartas/carta2.txt
+touch practica_compresion/documentos/informes/informe1.txt
+touch practica_compresion/documentos/informes/informe2.txt
+touch practica_compresion/imagenes/foto1.jpg
+touch practica_compresion/imagenes/foto2.jpg
+touch practica_compresion/listado.txt
+
+echo "Estructura de comodines extra creada correctamente."
